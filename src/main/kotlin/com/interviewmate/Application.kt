dea59ac8d5112075d3ca8abbf0a94d4db7d24db9
Maciej Szymeczko <maciek.047@gmail.com>
2025-07-30 19:58:33 +0200

    Add JPA entities and repositories

--- src/main/kotlin/com/interviewmate/Application.kt
+++ src/main/kotlin/com/interviewmate/Application.kt
@@ -2,9 +2,19 @@ package com.interviewmate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.boot.CommandLineRunner
+import com.interviewmate.repository.UserRepository
 
 @SpringBootApplication
-class InterviewMateApplication
+class InterviewMateApplication {
+
+    @Bean
+    fun initDatabase(userRepository: UserRepository): CommandLineRunner = CommandLineRunner {
+        val userCount = userRepository.count()
+        println("Database ready: user table rows = $userCount")
+    }
+}
 
 // Entry point for the Spring Boot application
 fun main(args: Array<String>) {
