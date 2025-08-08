6a2897470dfd0cdbfa7133ffc5c25f66601e1cd8
Maciej Szymeczko <maciek.047@gmail.com>
2025-08-08 11:28:08 +0000

    added missing import

--- src/test/kotlin/com/interviewmate/controller/InterviewControllerTest.kt
+++ src/test/kotlin/com/interviewmate/controller/InterviewControllerTest.kt
@@ -14,6 +14,7 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.Bean
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
